7f2d0462251c0881901f55b2f987ed08a8f27760
caelansar <819711623@qq.com>
2023-09-04 21:42:38 +0800

    feat(repl): experimentally introduce rustyline crate

--- src/repl/mod.rs
+++ src/repl/mod.rs
@@ -1,6 +1,15 @@
+use std::borrow::Cow;
 use std::io;
 
 use crate::{lexer, parser};
+
+use rustyline::completion::FilenameCompleter;
+use rustyline::error::ReadlineError;
+use rustyline::highlight::{Highlighter, MatchingBracketHighlighter};
+use rustyline::hint::HistoryHinter;
+use rustyline::validate::MatchingBracketValidator;
+use rustyline::{Cmd, Config, Editor, EventHandler, KeyEvent};
+use rustyline::{Completer, Helper, Hinter, Validator};
 
 #[cfg(feature = "vm")]
 pub fn repl<R: io::BufRead, W: io::Write>(mut reader: R, mut writer: W) -> io::Result<()> {
@@ -63,28 +72,99 @@ pub fn repl<R: io::BufRead, W: io::Write>(mut reader: R, mut writer: W) -> io::R
 
     writer.write_all(b"engine: interpreter\n")?;
 
+    let config = Config::builder().history_ignore_space(true).build();
+    let h = MyHelper {
+        completer: FilenameCompleter::new(),
+        highlighter: MatchingBracketHighlighter::new(),
+        hinter: HistoryHinter {},
+        colored_prompt: "".to_owned(),
+        validator: MatchingBracketValidator::new(),
+    };
+    let mut rl = Editor::with_config(config).unwrap();
+    rl.set_helper(Some(h));
+    rl.bind_sequence(KeyEvent::ctrl('n'), Cmd::HistorySearchForward);
+    rl.bind_sequence(KeyEvent::ctrl('p'), Cmd::HistorySearchBackward);
+    rl.bind_sequence(
+        KeyEvent::from('\t'),
+        EventHandler::Simple(Cmd::Insert(1, "\t".into())),
+    );
+
+    let mut count = 1;
     loop {
-        writer.write_all(b">>> ")?;
-        writer.flush()?;
+        let p = format!("{count}> ");
+        rl.helper_mut().expect("No helper").colored_prompt = format!("\x1b[1;32m{p}\x1b[0m");
+        let readline = rl.readline(&p);
+        match readline {
+            Ok(input) => {
+                rl.add_history_entry(input.as_str()).unwrap();
 
-        let mut input = String::new();
-        let size = reader.read_line(&mut input)?;
+                let lexer = lexer::Lexer::new(&input);
+                let mut parser = parser::Parser::new(lexer);
+                let program = parser.parse_program().unwrap();
 
-        let lexer = lexer::Lexer::new(&input);
-        let mut parser = parser::Parser::new(lexer);
-        let program = parser.parse_program().unwrap();
+                let obj = evaluator.eval(&program);
 
-        let obj = evaluator.eval(&program);
-
-        if size == 1 {
-            break;
+                match obj {
+                    Some(object::Object::Error(err)) => {
+                        writeln!(writer, "\x1b[41merror: {}\x1b[0m", err)?
+                    }
+                    Some(obj) => writeln!(writer, "< {}", obj)?,
+                    _ => {}
+                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("Goodbye!");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("Goodbye!");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {err:?}");
+                break;
+            }
         }
-        match obj {
-            Some(object::Object::Error(err)) => writeln!(writer, "\x1b[41merror: {}\x1b[0m", err)?,
-            Some(obj) => writeln!(writer, "< {}", obj)?,
-            _ => {}
+        count += 1;
+    }
+
+    Ok(())
+}
+
+#[derive(Helper, Completer, Hinter, Validator)]
+struct MyHelper {
+    #[rustyline(Completer)]
+    completer: FilenameCompleter,
+    highlighter: MatchingBracketHighlighter,
+    #[rustyline(Validator)]
+    validator: MatchingBracketValidator,
+    #[rustyline(Hinter)]
+    hinter: HistoryHinter,
+    colored_prompt: String,
+}
+
+impl Highlighter for MyHelper {
+    fn highlight_prompt<'b, 's: 'b, 'p: 'b>(
+        &'s self,
+        prompt: &'p str,
+        default: bool,
+    ) -> Cow<'b, str> {
+        if default {
+            Cow::Borrowed(&self.colored_prompt)
+        } else {
+            Cow::Borrowed(prompt)
         }
     }
-    writer.write_all(b"exit\n")?;
-    Ok(())
+
+    fn highlight_hint<'h>(&self, hint: &'h str) -> Cow<'h, str> {
+        Cow::Owned("\x1b[2m".to_owned() + hint + "\x1b[m")
+    }
+
+    fn highlight<'l>(&self, line: &'l str, pos: usize) -> Cow<'l, str> {
+        self.highlighter.highlight(line, pos)
+    }
+
+    fn highlight_char(&self, line: &str, pos: usize) -> bool {
+        self.highlighter.highlight_char(line, pos)
+    }
 }
